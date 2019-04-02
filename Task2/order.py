b33a08af97d45c03472ee2e4ddcab498ffec631b
KatrinaaDing <xiaoding1998@hotmail.com>
2019-04-02 23:31:30 +1100

    upload file

--- Task2/order.py
+++ Task2/order.py
@@ -1,5 +1,46 @@
 from food import food
 
+
+
+
 class Order():
-	def __init__():
-		
+	def __init__(self, orderId, food, orderStatus = "Not Submitted"):
+		self._orderId = orderId
+		self._orderedItem = food
+		self._orderStatus = orderStatus
+
+
+	# compute total price of the order
+	def computeNetPrice(self):
+		price = 0
+		for item in self.orderedItem:
+			price += item.price
+
+		return price
+
+
+	# new food list will overwrite the old food list
+	def modifyOrder(self, food = None):
+		assert(food != None)
+		self.orderedItem = food
+
+
+	# update orderStatus
+	def updateOrder(self, status):
+		self.orderStatus = status
+	''' 
+	Properties
+	'''
+
+	@property
+	def orderId(self):
+		return self._orderId
+	
+	@property
+	def orderedItem(self):
+		return self._orderedItem
+	
+	@property
+	def orderStatus(self):
+		return self._orderStatus
+	
--- Task2/ordering_system.py
+++ Task2/ordering_system.py
@@ -1,6 +1,16 @@
 from Food import Food
 from order import Order
+from stock import Stock
 
+
+class IdGenerator():
+
+	def __init__(self):
+		self._id = 0
+
+	def newId(self):
+		self._id += 1
+		return self._id
 
 class OrderingSystem():
 	def __init__(self):
@@ -9,21 +19,78 @@ class OrderingSystem():
 
 
 	def displayMenu(self):
+		for item in self.menu:
+			print(item)
+
+	'''
+	Function to make new order.
+	1. If no existed order passed in, make new one and checkStock. If some items are out of stock, return the order and set orderStatus as "Not Submitted", else, append it to order list and set status "Pending"
+	2. If an old order passed in, re-check stock. If there are still some items out of stock, return order; else, append it to order list and update status "Pending"
+
+	return value: order
+	'''
+	def makeOrder(self, old_order = None, food = None):
+		
+		assert(food != None)
+
+		if order==None:
+			newId = IdGenerator.newId()
+			emptyFood = checkStock(food)
+			if emptyFood:
+				new_order = Order(newID,food,"Not Submitted")
+			else:
+				new_order = Order(newID, food, "Pending")
+				self.order.append(new_order)
+				
+			return new_order
+
+		else:
+			assert(old_order != None)
+			emptyFood = checkStock(food)
+			if emptyFood:
+				old_order.modifyOrder(food)
+			else:
+				old_order.modifyOrder(food)
+				order.updateOrder("Pending")
+				self.order.append(old_order)
+
+			return old_order
+			
+	'''
+	Check if a list of food is out of stock
+	return value: list of out-of-store food
+	'''
+	def checkStock(self, order):
+		emptyFood = []
+
+		for item in order.orderedItem:
+			if item in Stock.mains:
+				if Stock.mains[item] == 0:
+					emptyFood.append(item)
+
+			elif item in  Stock.drinks:
+				if Stock.drinks[item] == 0:
+					emptyFood.append(item)
+
+			elif item in Stock.sides:
+				if Stock.sides[item] == 0:
+					emptyFood.append(item)
+
+			elif item in Stock.ingredients:
+				if Stock.ingredients[item] == 0:
+					emptyFood.append(item)
+
+		return emptyFood
 
 
-	def makeOrder(self):
-
-
-	def checkStock(self):
-
-
-	def updateOrder(self):
-
-
+	'''
+	Ask for customer if they would like to enter email address and then send a receipt
+	return value: None (void function)
+	'''
 	def sendReceipt(self):
 
 
-	def getNextOrder():
+	def getNextOrder(self, status = None, id = None):
 
 
 	def deleteOrder():
