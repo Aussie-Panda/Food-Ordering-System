968f63d7ad87e6f2d1d557a2baad469d737823f0
KatrinaaDing <xiaoding1998@hotmail.com>
2019-04-06 18:27:25 +1100

    add some tests

--- Task2/test_make_order.py
+++ Task2/test_make_order.py
@@ -1,51 +1,144 @@
 from ordering_system import OrderingSystem
-from mains import Mains
-from mains import Mains
+from food import Food
 from order import Order
 from stock import Stock
-from errors import StockError, QuantityError
+from errors import StockError, QuantityError, checkStock
+from mains import Mains, Burger, Wrap
+from drinks import Drinks
+from sides import Sides
+
 import pytest
 
 @pytest.fixture
 def sys():
 	sys = OrderingSystem()
 	sys.initMenu()
+	# initialise stock
+	for category in [sys.stock.mains, sys.stock.drinks, sys.stock.sides, sys.stock.ingredients]:
+		for item in category:
+			if  item ==  'Fries':
+				category[item]  = 100
+			elif 'Juice' in item:
+				category[item] = 1000
+			else:
+				category[item] = 10
+	# print(sys.stock)
+	# sys.displayMenu()
 	return sys
 
 def test_getSides(sys):
 	testSides = sys.getFood('Fries','med')
+	assert (testSides != None)
 	assert(testSides.name == 'Fries')
 	assert(testSides.size == 'med')
+	assert(testSides.volumn == 40)
+	assert(testSides.price ==  2)
+
+	s2 = sys.getFood('ddd', 'sml')
+	assert (s2 == None)
 
 def test_getMains(sys):
 	testMains = sys.getFood('Burger')
 	assert(testMains.name == 'Burger')
 	assert(testMains.price == 5)
 
+	m2 = sys.getFood('fff')
+	assert (m2 == None)
+
 def test_getDrinks(sys):
-	testDrinks = sys.getFood('Lemonade(Can)', 375)
-	assert(testDrinks.name == 'Lemonade(Can)')
+	testDrinks = sys.getFood('Lemonade', 'Cans')
+	assert(testDrinks.name == 'Lemonade')
 	assert(testDrinks.price == 3)
-	pass
+	assert(testDrinks.volumn == 375)
 
-# def test_make_order_with_exception(sys):
-# 	orderedFood = {sys.getFood('Nuggets','sml'): 3}
-# 	sys.stock.addQuantity('Nuggets', 2)
-# 	order, errors = sys.makeOrder(orderedFood)
-# 	assert(len(errors) != 0)
-# 	assert(order == None)
+	d2 = sys.getFood('Lemonade', 'sml')
+	assert(d2 == None)
 
+	d3 = sys.getFood('Orange_Juice', 'sml')
+	assert(d3.volumn == 250)
+
+	d4 = sys.getFood('Apple_Juice')
+	assert(d4 == None)
+	
 def test_make_order(sys):
-	orderedFood = {sys.getFood('Nuggets','sml'): 3}
-	s = sys.stock
-	s.changeQuantity('add', 'buns', 1)
-	s.changeQuantity('add', 'bottle', 1)
-	s.changeQuantity('add', 'can', 10)
-	s.changeQuantity('add', 'fries', 1, 'sml')
-	s.changeQuantity('add', 'nuggets', 1, 'lrg')
-	s.changeQuantity('add', 'juice', 1, 'lrg')
+	orderedFood = {sys.getFood('Nuggets','sml'): 3, sys.getFood('Lemonade', 'Cans'): 3}
 	order, errors = sys.makeOrder(orderedFood)
-	print(order)
+	assert(not errors)
+	assert(order.computeNetPrice() == 12)
+	assert(sys.stock.sides['Nuggets'] == 1)
+	assert(sys.stock.drinks['Lemonade(Cans)'] == 7 )
+
+def test_make_order_buger(sys):
+	burger = sys.getFood('Burger')
+	burger.addBuns(3)
+	burger.addPats(2)
+	burger.changeIngredients('tomato', 2)
+	burger.changeIngredients('cheddar_cheese', 3)
+	orderedFood = {burger: 1, sys.getFood('Orange_Juice','sml'): 2}
+	order, errors = sys.makeOrder(orderedFood)
+	# print(sys.stock)
+	assert(not errors)
+	assert(order.computeNetPrice() == 24)
+
+	assert(sys.stock.drinks['Orange_Juice'] == 500)
+	assert(sys.stock.ingredients['tomato'] == 8)
+	assert(sys.stock.ingredients['cheddar_cheese'] == 7)
+	assert(sys.stock.ingredients['buns'] == 7)
+	assert(sys.stock.ingredients['patties'] == 8)
+	assert(sys.stock.mains['Burger'] == 9)
+
+def test_make_order_wrap(sys):
+	wrap = sys.getFood('Wrap')
+	wrap.addPats(3)
+	wrap.changeIngredients('tomato_sauce', 3)
+	orderedFood = {wrap: 1, sys.getFood('Fries', 'lrg'): 1}
+	order, errors = sys.makeOrder(orderedFood)
+    
+	assert(not errors)
+	assert(order.computeNetPrice() == 16)
+
+	assert(sys.stock.sides['Fries'] == 40)
+	assert(sys.stock.mains['Wrap'] == 9)
+	assert(sys.stock.ingredients['tomato_sauce'] == 7)
+
+def test_out_of_stock(sys):
+	orderedFood = {sys.getFood('Nuggets', 'lrg'): 10, sys.getFood('Fries','sml'): 1}
+	order, errors = sys.makeOrder(orderedFood)
+	assert(len(errors) == 1 and 'Nuggets' in errors)
+	assert(order == None)
+
+	orderedFood = {sys.getFood('Orange_Juice', 'med'): 3, sys.getFood('Fries','sml'): 1}
+	order, errors = sys.makeOrder(orderedFood)
+	assert(len(errors) == 1 and 'Orange_Juice' in errors)
+	assert(order == None)
+
+	orderedFood = {sys.getFood('Lemonade', 'Bottles'): 11, sys.getFood('Fries','sml'): 1}
+	order, errors = sys.makeOrder(orderedFood)
+	assert(len(errors) == 1 and 'Lemonade(Bottles)' in errors)
+	assert(order == None)
+
+def test_modify_order(sys):
+	orderedFood = {sys.getFood('Nuggets','sml'): 3, sys.getFood('Fries','lrg'): 15 , sys.getFood('Fries','sml'): 8}
+	order, errors = sys.makeOrder(orderedFood)
+	assert(len(errors) == 1 and 'Fries' in errors)
+	assert(order == None)
+
+	sys.modifyOrder(orderedFood, sys.getFood('Fries', 'lrg'), 1)
+	sys.modifyOrder(orderedFood, sys.getFood('Fries', 'sml'), 1)
+	# expected orderedFood: {smlNuggets: 3, lrgFries: 1, smlFries: 1}
+	order, errors = sys.makeOrder(orderedFood)
+	assert(not errors)
+	assert(order.computeNetPrice() == 7)
+
+	print(sys.stock)
+	sys.modifyOrder(orderedFood, sys.getFood('Orange_Juice', 'med'), 2)
+	sys.modifyOrder(orderedFood, sys.getFood('Nuggets', 'sml'), 0)
+	sys.modifyOrder(orderedFood, sys.getFood('Fries', 'lrg'), 0)
+	# expected orderedFood: {smlFries: 1, medOrange_Juice: 2}	
+	assert(len(orderedFood) == 2)
+	order,errors = sys.makeOrder(orderedFood)
+	assert(not errors)
+	assert(order.computeNetPrice() == 9)
 
 # def test_modify_order_with_exception(sys):
 # 	orderedFood = {sys.getFood('Nuggets','sml'): 3}
@@ -62,7 +155,7 @@ def test_make_order(sys):
 	# fl2 = {sys.getFood('Fries','med'): 4, sys.getFood('Fries', 'sml'): 8, sys.getFood('Lemonade(Can)'): 9}
 
 	# testSides2 = sys.getFood('Fries','sml'): 10,sys.getFood('Fries','lrg'): 3,sys.getFood('Nuggets','sml'): 4,sys.getFood('Nuggets','lrg'): 4}
-	'''
+'''
 	# test makeOrder() and computeNetPrice()
 	o1, errors = sys.makeOrder(fl1)
 	assert(not errors)
@@ -103,6 +196,6 @@ def test_make_order(sys):
 	trashOrder = sys.deleteOrder("Ready")
 	trashOrder = sys.deleteOrder(None,1)
 	assert(not sys.order)
-	'''
+'''
 
 	
